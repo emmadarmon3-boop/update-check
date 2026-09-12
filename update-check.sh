6ec833f82211b90005e1264828eaf7380597cbc9
daemma <emma.darmon@ynov.com>
2026-09-12 12:00:11 +0200

    ajout du script de verification des mises à jours

--- update-check.sh
+++ update-check.sh
@@ -1,3 +1,14 @@
 #!/bin/bash 
 
-echo "je m'appelle"
+echo "=========================================="
+echo " Verification des mise a jours disponnible"
+echo "=========================================="
+
+
+RESULATS=$(apt list --upgradable  2>/dev/null)
+
+if [ -n "$RESULATS" ]; then 
+ 	echo "il y a une mise a jours"
+else 
+	echo "il n' y a pas de mise a jours"
+fi
